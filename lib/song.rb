a15fbd1473e114fbf0947cc6736435cc0046d301
whynotski <laurawynosky@gmail.com>
2018-07-23 17:00:07 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,55 +1,72 @@
+require 'pry'
+
 class Song
   @@count=0
   @@genres=[]
-  @@artist=[]
+  @@artists=[]
 @@artist_count={}
 @@genre_count={}
-@@all=[]
+#@@all=[]
 
   attr_accessor :name, :genre, :artist
-  @@count=0
-  @@artist=[]
-  @@genres=[]
+
 
   def initialize(name, artist, genre)
     @name=name
     @artist=artist
     @genre=genre
     @@count +=1
-    @@all << self
-  end
+    #@@all << self
+    @@genres << self.genre
+    @@artists << self.artist
+#emily set up all these keys and tests for counts
+      if @@artist_count.has_key?(@artist)
+          @@artist_count[artist] += 1
+        else
+          @@artist_count[@artist] = 1
+        end
+          if @@genre_count.has_key?(@genre)
+            @@genre_count[@genre] += 1
+          else
+            @@genre_count[@genre] = 1
+          end
+    end#close initialize method
 
-  def artist
+  def artist #getter for artist
     @artist
   end
 
-  def genre
+  def genre #getter for genre
     @genre
   end
 
   def self.count
-    #Total number of songs created
-    @count = @count + 1
-  end
-###################################################
-  def self.artists
-  @@artist.uniq
+   #Total number of songs created
+  @@count
+    # self.all.length
   end
 
-  def self.genres
-  @@genres.uniq
+  def self.all
+      @@all
+  end
+
+
+###################################################
+  def self.artists#Returns all artists in class
+    @@artists.uniq
+  end
+
+  def self.genres #RETURN ALL GENRES IN CLASS
+      @@genres.uniq
   end
 
   #count the genres and returns a hash {}
   def self.genre_count
-    Song.genre_count
-
+    @@genre_count
   end
 
   def self.artist_count
-    Song.artist_count
-  #   #Song.all.select do |song|
-  #     #song.artist == self
-end
+    @@artist_count
+  end
 
 end #class end
